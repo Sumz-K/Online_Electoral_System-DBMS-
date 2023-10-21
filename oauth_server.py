13ea62965b493ac33dec3699f360e181ef3168cb
Tejas A Kumar <109240619+Tejasakumar@users.noreply.github.com>
2023-10-21 14:32:31 +0530

    multi threaded server

--- oauth_server.py
+++ oauth_server.py
@@ -1,7 +1,6 @@
-from ctypes import resize
 from time import time
 from urllib import request
-
+import threading
 from flask import Flask, render_template, request, redirect, url_for,jsonify
 import mysql.connector
 import random
@@ -13,12 +12,51 @@ import datetime
 
 app=Flask(__name__)
 mysql_conn = mysql.connector.connect(
-    host = "192.168.0.102",
+    host = "172.20.10.4",
     user = "ubuntu",
     password = "pass"
 )
 cursor = mysql_conn.cursor()
 cursor.execute("use adhar;")
+
+
+
+def remove():
+    thread_cursor = mysql_conn.cursor()
+    thread_cursor.execute("USE adhar;")
+    while True:
+        
+        thread_cursor.execute("LOCK TABLES otp write")
+        try:
+            current = datetime.datetime.now()
+            thread_cursor.execute(f"DELETE FROM otp WHERE ttl < '{current}'")
+            mysql_conn.commit()
+        except mysql.connector.Error as err:
+            # Handle errors, print or log them as needed
+            print(f"Error: {err}")
+            mysql_conn.rollback()
+        finally:
+            thread_cursor.execute("UNLOCK Tables")
+        time.sleep(1)
+
+t = threading.Thread(target=remove)
+t.start()
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        data = request.form.get("uid",None)
+        cursor.execute("")
+
+    
+
+
+
+
+
+
+
+
 
 #App id, UID, time(current time)
 #Populate the other table 'token' : ID---> UID from details
@@ -31,28 +69,38 @@ def gettokens():
     cur_time=datetime.datetime.strptime(data["time"], '%Y-%m-%d %H:%M:%S.%f')
     #print(cur_time[0:19])
     uid=data["UID"]
-    cur_time1 = str(cur_time)
-    h=hashlib.sha256()
-    h.update((cur_time1+uid).encode())
+    print(uid)
+    q = "select * from details where uid = %s;"
+    v = (uid,)
+    cursor.execute(q,v)
+    see = cursor.fetchall()
+    print(see)
+    if len(see) != 0:
+        cur_time1 = str(cur_time)
+        h=hashlib.sha256()
+        h.update((cur_time1+uid).encode())
     
-    hashed=h.hexdigest()
+        hashed=h.hexdigest()
    
-    query="insert into sessions values(%s,%s,%s,%s)"
-    dynamic_ttl = cur_time + datetime.timedelta(minutes=10)
-    dynamic_ttl = str(dynamic_ttl)[:19]
-    values=(hashed,cur_time,dynamic_ttl,uid)
-    cursor.execute(query,values)
-    mysql_conn.commit()
+        query="insert into sessions values(%s,%s,%s,%s)"
+        dynamic_ttl = cur_time + datetime.timedelta(minutes=3)
+        dynamic_ttl = str(dynamic_ttl)[:19]
+        values=(hashed,cur_time,dynamic_ttl,uid)
+        cursor.execute(query,values)
+        mysql_conn.commit()
+        query = 'insert into otp values(%s,%s,%s)'
+        values = (uid,random.randint(1000,9999),str(cur_time+datetime.timedelta(minutes=3))[:19])
+        cursor.execute(query,values)
+        mysql_conn.commit()
+        dummy_dict={"status":"yes"}
+        return jsonify(dummy_dict),200
+    else:
+        ret = {"status":"no"}
+        return jsonify(ret),200
     
-
-    dummy_dict={"A":"B"}
-    return jsonify(dummy_dict),200
-
 def compare(lst):
     for i in range (len(lst)):
         lst[i]=lst[i][0]
-    
-
 
 @app.route("/check-otp",methods=['GET',"POST"])
 def otp_check():
@@ -65,14 +113,13 @@ def otp_check():
     val = (uid,)
     cursor.execute(query,val)
     val = cursor.fetchall()
-    
     compare(val)
     print(val,type(val[0]))
 
     if int(data['otp']) in val:
         return jsonify({"status":"accepted"}),200
-    return jsonify({"status":"rejected"}),200
 
+    return jsonify({"status":"rejected"}),200
 
 @app.route("/toapp",methods=['GET','POST'])
 def timetolive():
@@ -116,17 +163,10 @@ def getinfo():
     return (cursor.fetchall()),200
     
 
-@app.route('/printotp/<uid>',methods=['GET','POST'])
-def printotp(uid):
-    query=f"select code from otp where id={uid}"
-    cursor.execute(query)
-    res=cursor.fetchall()
-    print(res)
-    return res,200
-
+    
 
 
     
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
